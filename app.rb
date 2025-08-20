2130056dfa3dd4db090f9f7558994f71d9a0edb9
AB <35473718+arsalanbardsiri@users.noreply.github.com>
2025-08-19 22:24:47 -0700

    Game.new.run

--- app.rb
+++ app.rb
@@ -4,4 +4,4 @@ puts "Welcome To The Game Of"
 puts "#{R_P_S}"
 puts "Choose game mode: '1' for 2-Player, '2' for vs Computer"
 mode = gets.chomp
-Game.new(vs_computer: mode == "2").start
+Game.new(vs_computer: mode == "2").run
